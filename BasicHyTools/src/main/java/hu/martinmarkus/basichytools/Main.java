688e657188deff2aac205d511d1d38a0d9ee1ff2
Márkus Martin <martinmarkus0@gmail.com>
2019-05-15 17:21:09 +0200

    Add basic empty project

---
+++ BasicHyTools/src/main/java/hu/martinmarkus/basichytools/Main.java
@@ -0,0 +1,27 @@
+package hu.martinmarkus.basichytools;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import static com.sun.org.apache.xalan.internal.utils.SecuritySupport.getResourceAsStream;
+
+public class Main {
+    private static void printProjectProperties() {
+        try {
+            Properties properties = new Properties();
+            properties.load(getResourceAsStream("project.properties"));
+
+            String message = properties.getProperty("artifactId") + " v" + properties.getProperty("version")
+                    + " by '" + properties.getProperty("author") + "' is loaded."
+                    + "\nProject repository: " + properties.getProperty("repo");
+
+            System.out.println(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
